3b8829028ac8f640e6b35c74d41194698e6c5630
yifeng tang <yifengtang_hust@outlook.com>
2017-03-12 16:48:01 +0800

    Done without shuangpin and add_diy_dic.

--- main.cpp
+++ main.cpp
@@ -5,23 +5,27 @@
 #include "model/PinyinTireTree.h"
 
 int main() {
-    std::string s = "姨";
+    std::string s = "异";
     std::vector<char> v;
     v.insert(v.end(), s[0]);
     v.insert(v.end(), s[1]);
     v.insert(v.end(), s[2]);
     PinyinTireTree *tireTree = new PinyinTireTree;
     tireTree->initial();
-    std::map<std::vector<char>, double, ValueComparator> *map = tireTree->getCharacters("yi");
+    std::map<std::vector<char>, double, ValueComparator> *map = tireTree->getCharacters("y");
     std::map<std::string, double, WordsValueComparator> *wordMap = tireTree->chooseCharacter(v);
     tireTree->chooseWord(NOT_CHOOSE_S);
     std::map<std::vector<char>, double, ValueComparator>::iterator it;
+    std::map<std::string, double, WordsValueComparator>::iterator wordIt;
     for (it = map->begin(); it != map->end(); it++) {
         std::string *test = new std::string;
         test->insert(test->end(), it->first.at(0));
         test->insert(test->end(), it->first.at(1));
         test->insert(test->end(), it->first.at(2));
         std::cout << *test << " " << it->second << std::endl;
+    }
+    for (wordIt = wordMap->begin(); wordIt != wordMap->end(); wordIt++) {
+        std::cout << wordIt->first << " " << wordIt->second << std::endl;
     }
 
 }
