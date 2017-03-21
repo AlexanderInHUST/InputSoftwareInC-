eedbeb9c7816e9656dcc9d0fb44e9b0a0e7baebb
yifeng tang <yifengtang_hust@outlook.com>
2017-03-21 16:43:56 +0800

    Test Done.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "model/PinyinTrieTree.h"
 
 int main() {
-    std::string s = "浆";
+    std::string s = "江";
     std::vector<char> v;
     v.insert(v.end(), s[0]);
     v.insert(v.end(), s[1]);
@@ -25,4 +25,28 @@ int main() {
     for(int i = 0; i < wordMap->size(); i++) {
         std::cout << *(wordMap->at(i)->self) << " " << wordMap->at(i)->value << std::endl;
     }
+
+    tireTree->finishInput();
+
+    s = "奖";
+    v.clear();
+    v.insert(v.end(), s[0]);
+    v.insert(v.end(), s[1]);
+    v.insert(v.end(), s[2]);
+    map = tireTree->getCharacters("j");
+    wordMap = tireTree->chooseCharacter(v);
+    tireTree->chooseWord(NOT_CHOOSE_S);
+    for(int i = 0; i < map->size(); i++) {
+        std::string *test = new std::string;
+        test->insert(test->end(), map->at(i)->self->at(0));
+        test->insert(test->end(), map->at(i)->self->at(1));
+        test->insert(test->end(), map->at(i)->self->at(2));
+        std::cout << *test << " " << map->at(i)->value << std::endl;
+    }
+    for(int i = 0; i < wordMap->size(); i++) {
+        std::cout << *(wordMap->at(i)->self) << " " << wordMap->at(i)->value << std::endl;
+    }
+
+    tireTree->finishInput();
+    tireTree->close();
 }
